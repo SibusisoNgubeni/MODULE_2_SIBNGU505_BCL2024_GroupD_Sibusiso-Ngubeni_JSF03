9bfa69d484b1e50e7435836232a5f86c2dc52cf8
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-08-01 10:28:08 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+<template>
+
+<nav class="container">
+      <div>
+           <img src="../assets/planet-5-logo.png" class="logo" alt="planet Logo">
+      </div>
+</nav>
+</template>
+
+<style>
+
+.container{
+    background-color: #2b3843;
+    height: 72px;
+    border-radius: 10px;
+    padding: 5px;
+ }
+
+ .logo{
+    height: 60px;
+    width: 150px;
+    margin-left: 20px;
+    border-radius: 5px;
+ }
+
+ .logo-text{
+    color: rgb(56, 97, 131);
+    font-size: 30px;
+    font-weight: 600;
+    margin-left: 20px;
+ }
+</style>
